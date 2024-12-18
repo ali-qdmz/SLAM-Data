5c018a92e6d2da6e096b9acb86bdb1e5f2811d89
Ali <ali.ghadimzadeh@gmail.com>
2024-12-18 11:44:25 -0500

    Add files via upload

---
+++ mine dataset/LIO_final/resource.py
@@ -0,0 +1,47 @@
+import random
+from time import time
+import pandas as pd
+import os
+import math
+from sklearn.metrics import mean_squared_error 
+import matplotlib.pyplot as plt
+import os
+
+directory_myself = "/home/ali/bagfiles (copy)/LIO_final"
+df_cpu_1 = pd.read_csv(directory_myself + '/_slash_cpu_monitor_slash_lio_sam_featureExtraction_slash_cpu.csv')
+df_cpu_2 = pd.read_csv(directory_myself + '/_slash_cpu_monitor_slash_lio_sam_imageProjection_slash_cpu.csv')
+df_cpu_3 = pd.read_csv(directory_myself + '/_slash_cpu_monitor_slash_lio_sam_imuPreintegration_slash_cpu.csv')
+df_cpu_4 = pd.read_csv(directory_myself + '/_slash_cpu_monitor_slash_lio_sam_mapOptmization_slash_cpu.csv')
+
+
+
+df_mem_1 = pd.read_csv(directory_myself + '/_slash_cpu_monitor_slash_lio_sam_imageProjection_slash_mem.csv')
+df_mem_2 = pd.read_csv(directory_myself + '/_slash_cpu_monitor_slash_lio_sam_imuPreintegration_slash_mem.csv')
+df_mem_3 = pd.read_csv(directory_myself + '/_slash_cpu_monitor_slash_lio_sam_mapOptmization_slash_mem.csv')
+
+
+df_cpu_total = df_cpu_1['data']/3 + df_cpu_2['data']/3 + df_cpu_3['data']/3  + df_cpu_4['data']/3
+
+df_cpu_total = df_cpu_total.iloc[0:len(df_cpu_total):2]
+ax = df_cpu_total.plot()
+ax.set_xlim(0, len(df_cpu_total))
+
+plt.xlabel('time, [seconds]')
+plt.ylabel('usage, [percent]')
+plt.savefig(directory_myself + '/data/cpu_usage.png')
+plt.show()
+
+df_mem_total = df_mem_1['data'] + df_mem_2['data'] + df_mem_3['data']
+
+df_mem_total = df_mem_total.iloc[0:len(df_mem_total):2]
+print(len(df_mem_total))
+ax = df_mem_total.plot()
+ax.set_xlim(0, len(df_mem_total))
+
+df_mem_total.plot()
+plt.xlabel('time, [seconds]')
+plt.ylabel('usage, [bits]')
+plt.savefig(directory_myself + '/data/mem_usage.png')
+plt.show()
+
+print(df_cpu_total.mean(), df_mem_total.mean())
